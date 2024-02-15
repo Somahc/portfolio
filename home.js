d7dfd60423a9e82dffc18c403e6068f0bfb90116
Soma Sekine <a8121151@aoyama.jp>
2024-02-15 15:01:04 -0500

    Skill欄追加

--- home.js
+++ home.js
@@ -1,7 +1,31 @@
 const careerElement = document.getElementById('career');
+const workElement = document.getElementById('work');
+const skillElement = document.getElementById('skill');
+const profileElement = document.getElementById('profile');
+const homeElement = document.getElementById('home');
 
-function navCareerClick() {
+navCareerClick = () => {
     careerElement.scrollIntoView({  
+        behavior: 'smooth'  
+    });
+}
+navWorkClick = () => {
+    workElement.scrollIntoView({  
+        behavior: 'smooth'  
+    });
+}
+navSkillClick = () => {
+    skillElement.scrollIntoView({  
+        behavior: 'smooth'  
+    });
+}
+navProfileClick = () => {
+    profileElement.scrollIntoView({  
+        behavior: 'smooth'  
+    });
+}
+navHomeClick = () => {
+    homeElement.scrollIntoView({  
         behavior: 'smooth'  
     });
 }
